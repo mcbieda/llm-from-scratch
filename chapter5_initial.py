71bad476180215c394b652c48d74b4ef1fc4ee12
Mark Bieda <mark.extrastuff1@gmail.com>
2025-07-29 23:54:57 -0700

    added chapter 2 dataloader and also moved the-verdict to data

--- chapter5_initial.py
+++ chapter5_initial.py
@@ -431,3 +431,63 @@ token_ids = generate_text_simple(
 )
 print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
 
+# %%
+# LOAD THE VERDICT
+filenm = "/home/markb/llm-from-scratch/data/the-verdict.txt"
+with open(filenm, 'r', encoding='utf-8') as f:
+    text_data = f.read()
+
+# %%
+# check length
+total_characters = len(text_data)
+total_tokens = len(tokenizer.encode(text_data))
+print("Characters:", total_characters)
+print("Tokens:", total_tokens)
+
+# %%
+# setup train-test split
+train_ratio=0.90
+train_index = int(train_ratio *total_characters)
+train_data = text_data[:train_index]
+val_data = text_data[train_index:]
+
+# %%
+# dataloader from chapter 2
+import torch
+from torch.utils.data import Dataset, DataLoader
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+    
+    
+def create_dataloader_v1(txt, batch_size=4, max_length=256,
+                         stride=128, shuffle=True, drop_last=True,
+                         num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
+
+# %%
