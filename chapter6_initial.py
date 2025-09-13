3370ae6579cc5d982505fda4747f2468693258d3
Mark Bieda <mark.extrastuff1@gmail.com>
2025-09-13 12:23:46 -0700

    some adjustments to the chapter6_initial

--- chapter6_initial.py
+++ chapter6_initial.py
@@ -8,32 +8,36 @@ import zipfile
 import os
 from pathlib import Path
 
-url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
-zip_path = "sms_spam_collection.zip"
+# ------------------
+# UNCOMMENT below to get the data - I have already gotten the data
+# ------------------
+
+# url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
+# zip_path = "sms_spam_collection.zip"
 extracted_path = "sms_spam_collection"
 data_file_path = Path(extracted_path) / "SMSSpamCollection.tsv"
 
 
-def download_and_unzip_spam_data(
-        url, zip_path, extracted_path, data_file_path):
-    if data_file_path.exists():
-        print(f"{data_file_path} already exists. Skipping download "
-              "and extraction."
-        )
-        return
+# def download_and_unzip_spam_data(
+#         url, zip_path, extracted_path, data_file_path):
+#     if data_file_path.exists():
+#         print(f"{data_file_path} already exists. Skipping download "
+#               "and extraction."
+#         )
+#         return
 
-    with urllib.request.urlopen(url) as response:
-        with open(zip_path, "wb") as out_file:
-            out_file.write(response.read())
+#     with urllib.request.urlopen(url) as response:
+#         with open(zip_path, "wb") as out_file:
+#             out_file.write(response.read())
 
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:
-        zip_ref.extractall(extracted_path)
+#     with zipfile.ZipFile(zip_path, "r") as zip_ref:
+#         zip_ref.extractall(extracted_path)
 
-    original_file_path = Path(extracted_path) / "SMSSpamCollection"
-    os.rename(original_file_path, data_file_path)
-    print(f"File downloaded and saved as {data_file_path}")
+#     original_file_path = Path(extracted_path) / "SMSSpamCollection"
+#     os.rename(original_file_path, data_file_path)
+#     print(f"File downloaded and saved as {data_file_path}")
 
-download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
+# download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
 
 # %%
 # LOAD DATA FROM FILE (if above already done)
@@ -89,7 +93,7 @@ validation_df.to_csv("validation.csv", index=None)
 test_df.to_csv("test.csv", index=None)
 
 # %%
-# check on end of text
+# CHECK on end of text
 import tiktoken
 tokenizer = tiktoken.get_encoding("gpt2")
 print(tokenizer.encode("<|endoftext|>", allowed_special={"<|endoftext|>"}))
@@ -162,6 +166,7 @@ test_dataset = SpamDataset(
     max_length=train_dataset.max_length,
     tokenizer=tokenizer
 )
+
 # %%
 # SETUP DATALOADERS
 from torch.utils.data import DataLoader
@@ -214,5 +219,6 @@ model_configs = {
     "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
 }
 BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
 # %%
 # LISTING 6.6 ADD HERE
