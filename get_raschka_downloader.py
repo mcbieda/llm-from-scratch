b401baf972c3012880858e56f8c8ca5bb0d3e749
Mark Bieda <mark.extrastuff1@gmail.com>
2025-10-05 14:33:52 -0700

    updated the get_raschka_downloader.py to allow me to get various gpt2 models - like the small, medium, etc and also updated naming of pickle files that I create

--- get_raschka_downloader.py
+++ get_raschka_downloader.py
@@ -9,22 +9,47 @@ url = (
 filename = url.split('/')[-1]
 urllib.request.urlretrieve(url, filename)
 
+
+# %%
+# %% SET MODEL SIZE TO DOWNLOAD
+
+# OPTIONS HERE
+# model_configs = {
+#     "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+#     "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+#     "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+#     "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+# }
+model_size = "355M"
+
+
 # %%
 from gpt_download import download_and_load_gpt2
 settings, params = download_and_load_gpt2(
-    model_size="124M", models_dir="gpt2"
+    model_size=model_size, models_dir="gpt2_all"
 )
 # %%
+# PRINT settings and parameter keys
 print("Settings:", settings)
 print("Parameter dictionary keys:", params.keys())
+
+# %%
+# CREATE file names for pickle
+
+# create names
+settings_nm = "gpt2_openai_settings_" + model_size + ".pkl"
+parameters_nm = "gpt2_openai_params_" + model_size + ".pkl"
+
 # %%
 # save settings dictionaries using pickle
 import pickle
-with open("gpt2_openai_settings.pkl", "wb") as f:
+with open(settings_nm, "wb") as f:
     pickle.dump(settings, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
 # %%
 # save params dictionary using pickle
-with open("gpt2_openai_params.pkl", "wb") as f:
+with open(parameters_nm, "wb") as f:
     pickle.dump(params, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 # %%
